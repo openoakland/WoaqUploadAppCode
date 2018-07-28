f26b84165b13cbb94c892438aee8f8e8ab5316a9
JENNIFER JUO <jenjuo@JENNIFERs-MacBook-Air-3.local>
2018-07-28 11:48:48 -0700

    Added html page

--- web.py
+++ web.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+#@app.route('/')
+#def hello_world():
+#    return 'Hello, World!'
